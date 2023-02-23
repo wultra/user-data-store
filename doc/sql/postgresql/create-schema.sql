a259ef51591b91fd0b5e78d24e809732c6a29ddd
Lubos Racansky <lubos.racansky@gmail.com>
2023-02-23 15:27:43 +0100

    Schema for Spring Security

--- doc/sql/postgresql/create-schema.sql
+++ doc/sql/postgresql/create-schema.sql
@@ -6,3 +6,20 @@ CREATE TABLE public.ud_user_claims
     timestamp_updated TIMESTAMP WITHOUT TIME ZONE,
     CONSTRAINT ud_user_claims_pkey PRIMARY KEY (user_id)
 );
+
+-- Spring Security
+CREATE TABLE ud_users
+(
+    username VARCHAR(50) NOT NULL PRIMARY KEY,
+    password VARCHAR(500) NOT NULL,
+    enabled  BOOLEAN NOT NULL
+);
+
+create table ud_authorities
+(
+    username  VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES ud_users (username)
+);
+
+CREATE UNIQUE INDEX ix_auth_username ON ud_authorities (username, authority);
