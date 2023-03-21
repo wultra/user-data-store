753c422f1bf3ddd6ebf34debd18c0847d1b65156
Lubos Racansky <lubos.racansky@gmail.com>
2023-03-21 09:35:23 +0100

    Fix #24: Update to Spring Boot 3

--- src/main/java/com/wultra/security/userdatastore/userclaims/UserClaimsEntity.java
+++ src/main/java/com/wultra/security/userdatastore/userclaims/UserClaimsEntity.java
@@ -17,10 +17,10 @@
  */
 package com.wultra.security.userdatastore.userclaims;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
