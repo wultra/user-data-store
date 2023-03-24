6d56826f47ac8dfd8ccbce022004d7b257f6affc
Lubos Racansky <lubos.racansky@gmail.com>
2023-03-24 09:08:26 +0100

    Fix #31: Exception encountered during context initialization

--- src/main/java/com/wultra/security/userdatastore/WebSecurityConfiguration.java
+++ src/main/java/com/wultra/security/userdatastore/WebSecurityConfiguration.java
@@ -22,6 +22,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,6 +33,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * @author Lubos Racansky lubos.racansky@wultra.com
  */
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration {
 
     @Value("${user-data-store.security.basic.realm}")
