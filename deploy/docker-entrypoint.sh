ec9ca7bf8e00931432d3cf147a2593e2512b42a2
Martin Korbel <martin@pd.lc>
2024-08-01 15:41:28 +0200

    Fail docker start if any of docker-entrypoint.sh commands fails

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -euo pipefail
 
 liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/db/changelog/db.changelog-master.xml \
-  --username=$USER_DATA_STORE_DATASOURCE_USERNAME \
-  --password=$USER_DATA_STORE_DATASOURCE_PASSWORD \
-  --url=$USER_DATA_STORE_DATASOURCE_URL \
+  --username="${USER_DATA_STORE_DATASOURCE_USERNAME:-}" \
+  --password="${USER_DATA_STORE_DATASOURCE_PASSWORD:-}" \
+  --url="${USER_DATA_STORE_DATASOURCE_URL}" \
   update
 
 java -jar user-data-store.war
