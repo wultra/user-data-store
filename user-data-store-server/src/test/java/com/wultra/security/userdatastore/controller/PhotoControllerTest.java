7b6900ba3e0c1ae291aadabeb5aaf06f7b3600a5
Lubos Racansky <lubos.racansky@gmail.com>
2025-05-30 11:34:48 +0200

    Fix #294: Replace MockBean with MockitoBean

--- user-data-store-server/src/test/java/com/wultra/security/userdatastore/controller/PhotoControllerTest.java
+++ user-data-store-server/src/test/java/com/wultra/security/userdatastore/controller/PhotoControllerTest.java
@@ -27,10 +27,10 @@ import io.getlime.core.rest.model.base.request.ObjectRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.imageio.ImageIO;
@@ -70,7 +70,7 @@ class PhotoControllerTest {
         }
     }
 
-    @MockBean
+    @MockitoBean
     private PhotoService service;
 
     @Autowired
