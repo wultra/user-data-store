04b720765e519ed1b5d7147d0c7a70f130e3c22b
Lubos Racansky <lubos.racansky@gmail.com>
2025-08-13 10:57:20 +0200

    Fix #322: OTEL instrumentation does not work for docker deployment

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -7,4 +7,4 @@ liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/db/changelog
   --url="${USER_DATA_STORE_DATASOURCE_URL}" \
   update
 
-java -jar user-data-store.war
+java ${JAVA_OPTS:-} -jar user-data-store.war
