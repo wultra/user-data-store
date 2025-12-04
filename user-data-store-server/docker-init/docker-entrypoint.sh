92384d493c93e7894a19dd983133c5e49474b2d6
Filip Bursik <filip.bursik@wultra.com>
2025-12-04 18:41:15 +0200

    Fix file paths for Liquibase changelog references in Docker initialization script and workflow

--- user-data-store-server/docker-init/docker-entrypoint.sh
+++ user-data-store-server/docker-init/docker-entrypoint.sh
@@ -4,7 +4,7 @@ KEEP_RUNNING=${KEEP_RUNNING:-false}
 KEEP_RUNNING_PORT=${KEEP_RUNNING_PORT:-666}
 
 liquibase --headless=true --log-level=INFO \
-    --changeLogFile="changelog/user-data-store-server/db.changelog-module.xml" \
+    --changeLogFile="changelog/user-data-store/db.changelog-module.xml" \
     --username="${POWERAUTH_SERVER_DATASOURCE_USERNAME:-}" \
     --password="${POWERAUTH_SERVER_DATASOURCE_PASSWORD:-}" \
     --url="${POWERAUTH_SERVER_DATASOURCE_URL}" \
