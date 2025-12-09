a826a5c47f60d44a453c46ec9864f73c38e520ee
Filip Bursik <filip.bursik@wultra.com>
2025-12-09 15:51:24 +0200

    Fix conditional check for LQ_ENABLED in Docker entrypoint script

--- user-data-store-server/docker/docker-entrypoint.sh
+++ user-data-store-server/docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ "${LQ_ENABLED}" = true ]; then
+if [ "${LQ_ENABLED}" = "true" ]; then
   liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/db/changelog/db.changelog-master.xml \
     --username="${USER_DATA_STORE_DATASOURCE_USERNAME:-}" \
     --password="${USER_DATA_STORE_DATASOURCE_PASSWORD:-}" \
