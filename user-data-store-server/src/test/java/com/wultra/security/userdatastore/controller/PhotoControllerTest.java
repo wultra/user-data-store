af732bd840920e0c485be0b850d1d0a840dc1e6c
Roman Strobl <roman.strobl@wultra.com>
2026-05-14 09:41:22 +0200

    Fix #417: Support Spring Boot 4

--- user-data-store-server/src/test/java/com/wultra/security/userdatastore/controller/PhotoControllerTest.java
+++ user-data-store-server/src/test/java/com/wultra/security/userdatastore/controller/PhotoControllerTest.java
@@ -26,7 +26,7 @@ import com.wultra.security.userdatastore.service.PhotoService;
 import com.wultra.core.rest.model.base.request.ObjectRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
--- user-data-store-server/src/test/java/com/wultra/security/userdatastore/controller/UserClaimsControllerTest.java
+++ user-data-store-server/src/test/java/com/wultra/security/userdatastore/controller/UserClaimsControllerTest.java
@@ -21,7 +21,7 @@ import com.wultra.security.userdatastore.config.WebSecurityConfiguration;
 import com.wultra.security.userdatastore.service.UserClaimsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
