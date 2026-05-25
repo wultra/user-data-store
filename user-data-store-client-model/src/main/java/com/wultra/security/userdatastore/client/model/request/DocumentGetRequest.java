85b056ec035980f319cfa4f6098ac121c8fa3393
Lubos Racansky <lubos.racansky@gmail.com>
2026-05-25 11:58:46 +0200

    copyright year

--- user-data-store-client-model/src/main/java/com/wultra/security/userdatastore/client/model/request/DocumentGetRequest.java
+++ user-data-store-client-model/src/main/java/com/wultra/security/userdatastore/client/model/request/DocumentGetRequest.java
@@ -1,6 +1,6 @@
 /*
  * User Data Store
- * Copyright (C) 2024 Wultra s.r.o.
+ * Copyright (C) 2026 Wultra s.r.o.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
